151bb5f284dd0b4399ad5014b02dfdfd1ddfd5d3
jhamlin461 <theburrito461@gmail.com>
2022-09-26 19:26:20 -0400

    Added to IntelliJ

---
+++ Hello_World/src/Main.java
@@ -0,0 +1,17 @@
+/*
+      Name:  Joshua Hamlin
+     Class: CSET 3600
+  Homework: 1 | Hello_World
+      Date: 9/14/2022
+   Summary: This program displays the text ""Hello World!"
+*/
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
